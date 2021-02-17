77deb104111549c594531091ab36954781be94e9
Nikola Vojinovic <61520374+nikolaVojinovic7@users.noreply.github.com>
2021-02-17 13:33:27 -0500

    Delete Documents/School/year3/semester2/CapstoneII/capstoneProject directory

--- Documents/School/year3/semester2/CapstoneII/capstoneProject/src/main/java/com/capstone/project/services/RecipeServiceAbs.java
+++
@@ -1,7 +0,0 @@
-package com.capstone.project.services;
-import com.capstone.project.model.Recipe;
-
-
-public interface RecipeServiceAbs extends CrudService<Recipe, Long>{
-    Recipe save(Recipe recipe);
-}
